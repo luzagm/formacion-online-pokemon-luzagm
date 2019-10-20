4ee43a68e72d043c7326df3544a6023a17fb016a
luzagm <luz.aguirre.mo@gmail.com>
2019-10-20 15:22:44 +0200

    run build

---
+++ docs/precache-manifest.891a7060800082e571a733e26dfe3b2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba5039251199889a8787c575349c243",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bbd587b2da68775d74c2",
+    "url": "./static/css/main.e73a7105.chunk.css"
+  },
+  {
+    "revision": "6d0741a956e7d1496343",
+    "url": "./static/js/2.04a85b6c.chunk.js"
+  },
+  {
+    "revision": "bbd587b2da68775d74c2",
+    "url": "./static/js/main.6abd4272.chunk.js"
+  },
+  {
+    "revision": "877a035caeacfb77bd89",
+    "url": "./static/js/runtime-main.34392fff.js"
+  },
+  {
+    "revision": "c84553efaecfcb6576fc6a62cdb9d00b",
+    "url": "./static/media/background.c84553ef.png"
+  },
+  {
+    "revision": "ddd5084dff8d7606019645f62c1712c6",
+    "url": "./static/media/pokemon.ddd5084d.png"
+  }
+]);
